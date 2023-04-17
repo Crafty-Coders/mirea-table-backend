a68ff2b59a86d2d78a51a2db48a48913da3b51b2
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2023-04-17 15:17:35 +0300

    initial commit

---
+++ src/main/java/com/team120hz/mireatab/MireaTabApplication.java
@@ -0,0 +1,13 @@
+package com.team120hz.mireatab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireaTabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MireaTabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team120hz/mireatab/MireaTabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team120hz.mireatab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireaTabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
